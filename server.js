1a3953670c71141aab1d9de8ba619642c954c1f1
Ravi <ravi.sindhi@daffodilsw.com>
2017-06-26 13:24:52 +0530

    commit server js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'),
   bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Tododb');
+
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+  config.db.mongo.url = connection_string;
+}
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +31,12 @@ var routes = require('./api/routes/todoListRoutes');
 routes(app);
 
 
-app.listen(port);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-console.log('todo list RESTful API server started on: ' + port);
+app.listen(server_port, server_ip_address, function () {
+
+    console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
+
+});
