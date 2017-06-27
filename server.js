861df6fefbb863567a84d37f0b934d86cf6968a7
Ravi Shekhar <rshekhar.in@gmail.com>
2017-06-27 18:49:47 +0530

    Updated db_url

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 51010,
+  db_url = process.env.DATABASE_URL,
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'),
   bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://heroku_2t7xmcsx:b69padm1ni2pbrrbs5r5hvkn50@ds139362.mlab.com:39362/heroku_2t7xmcsx');
+mongoose.connect(db_url);
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,4 +25,4 @@ routes(app);
 app.listen(port);
 
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log('todo list RESTful API server started on: ' + port + ' with db ' + db_url);
