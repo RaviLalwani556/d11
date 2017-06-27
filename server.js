8344e508ab4b4793ee683d5d19e8fba26e316140
Ravi Shekhar <rshekhar.in@gmail.com>
2017-06-27 17:12:27 +0530

    Updated PORT for running server

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ var routes = require('./api/routes/todoListRoutes');
 routes(app);
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
 
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
